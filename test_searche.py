f3f3470342583007b8f095547e9f983b54e8b87a
unknown <xxxascent@rambler.ru>
2024-09-13 15:39:47 +0600

    последний

--- test_searche.py
+++ test_searche.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
 import time
 import json
 
+#загружаем произвольные данные для регистрации из файлов json
 with open('user.json',"r",encoding="utf-8") as f:
     user=json.loads(f.read())
 
@@ -14,39 +12,40 @@ with open('order.json',"r",encoding="utf-8") as f:
 
 driver = webdriver.Chrome()
 
+# ОБОРАЧИВАЕМ все наши манипуляции в блок try, при возникновении ошибки на консоле будет исключение
 try: 
     driver.get("https://www.saucedemo.com")
-    #behave -i  my_feature.feature --no-capturedriver.find_element(By.CLASS_NAME,"user-name")
     time.sleep(1)
 
+#Вводим имя в поле логин
     login=driver.find_element(By.NAME,"user-name")
     login.clear()
     login.send_keys(user["login"])
     time.sleep(1)
-
+#Вводим пароль в поле пароль
     password=driver.find_element(By.NAME,"password")
     password.clear()
     password.send_keys(user["password"])
     time.sleep(1)
-
+#Нажимаем кнопку регшистрации
     button=driver.find_element(By.NAME,"login-button").click()
     time.sleep(1)
-
+#Выбираем товар "Sauce Labs Backpack"
     button=driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
     time.sleep(2)
-
+#Заходим в корзину
     button=driver.find_element(By.CLASS_NAME,"shopping_cart_link").click()
     time.sleep(2)
-
+#Убуждаемся в наличии товара в корзине
     driver.find_element(By.ID,"item_4_title_link")
     time.sleep(2)
-
+#Жмем кнопку "checkout"
     button=driver.find_element(By.ID,"checkout").click()
     time.sleep(2)
-
+#Убеждаемся в появлении формы оформления заказа
     driver.find_element(By.CLASS_NAME,"checkout_info")
     time.sleep(2)
-
+#Заполняем поля из файла json
     password=driver.find_element(By.NAME,"firstName")
     password.clear()
     password.send_keys(order["firstName"])
@@ -61,10 +60,10 @@ try:
     password.clear()
     password.send_keys(order["postal-Code"])
     time.sleep(1)
- 
+ #Оформляем покупку
     button=driver.find_element(By.NAME,"continue").click()
     time.sleep(2)
-
+#Проверяем покупку
     driver.find_element(By.CLASS_NAME,"summary_info_label")
     time.sleep(2)
 
